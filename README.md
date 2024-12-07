9a36f5cfe5ee274841c4db48785bfc3676212fae
plboilingNL <mwlodarski@icloud.com>
2024-12-08 00:21:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nuxt 3 contact form with validation and feedback
+
+Form component has name, email and TOS check validation and feedback. Uses setTimeout() to mimic form submission.
+
+Uses Tailwind CSS. 
+
+---
+
+Connect with me on LinkedIn at https://www.linkedin.com/in/hellomichaelsynan
---
+++ components/feedback.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+const props = defineProps({
+  formFeedback: String,
+});
+</script>
+
+<template>
+  <div
+    v-if="formFeedback === 'error'"
+    class="text-white w-full block p-3 mt-4 bg-red-500"
+  >
+    There was an error processing your request.
+  </div>
+
+  <div
+    v-else-if="formFeedback === 'success'"
+    class="text-white w-full block p-3 mt-4 bg-green-500"
+  >
+    Thanks for signing up!
+  </div>
+
+  <div
+    v-else-if="formFeedback === 'incomplete'"
+    class="text-white w-full block p-3 mt-4 bg-red-500"
+  >
+    Please complete all required fields.
+  </div>
+
+  <div
+    v-else-if="formFeedback === 'invalid'"
+    class="text-white w-full block p-3 mt-4 bg-red-500"
+  >
+    Please enter a valid email address.
+  </div>
+
+  <div
+    v-else-if="formFeedback === 'consent'"
+    class="text-white w-full block p-3 mt-4 bg-red-500"
+  >
+    Please agree to terms and conditions.
+  </div>
+</template>
+
+<style scoped></style>
